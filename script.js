164dd5388f3cdf569f85bcb5383a2cb4ff1e51c9
qzwxas83 <132167934+qzwxas83@users.noreply.github.com>
2023-10-08 16:26:28 +0300

    0.0.3

--- script.js
+++ script.js
@@ -23,32 +23,10 @@ let gameZone = document.querySelector('.game-zone'),
         width: 78,
         height: 77,
     }
-    player2 = {
-        bullet2: {
-            speed: 3.23
-        },
-        sprites: {
-        top: 'img/player2-top.png',
-        bottom: 'img/player2-bottom.png',
-        right: 'img/player2-right.png',
-        left: 'img/player2-left.png',
-        },
-        el: false,
-        x: 500,
-        y: 30,
-        step: 2,
-        run2: false,
-        side: 3, //1 (top), 2 (right), 3 (bottom), 4 (left)
-        width: 78,
-        height: 77,
-    }
-
+   
 function init() {
     gameZone.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
-    gameZone.innerHTML += `<div class="player2" style="left: ${player2.x}px; top: ${player2.y}px"></div>`; 
     player.el = document.querySelector('.player')
-    player2.el = document.querySelector('.player2')
-
 }
 
 /*
@@ -87,41 +65,6 @@ function intervals() {
                         player.el.style.backgroundImage = `url(${player.sprites.left})`;
                         player.x -= player.step;
                         player.el.style.left = `${player.x}px`;
-                    }
-                    break;
-                    
-            }
-        }
-    }, fps);
-    ints.run = setInterval(() => {
-        if (player2.run2) {
-            switch (player2.side) {
-                case 1: // top
-                    if (player2.y > 0) {
-                        player2.el.style.backgroundImage = `url(${player2.sprites.top})`;
-                        player2.y -= player2.step;
-                        player2.el.style.top = `${player2.y}px`;
-                    }
-                    break;
-                case 3: // bottom
-                if (player2.y < gameZone.getBoundingClientRect().bottom - player2.height - 2) {
-                    player2.el.style.backgroundImage = `url(${player2.sprites.bottom})`;
-                    player2.y += player2.step;
-                    player2.el.style.top = `${player2.y}px`;
-                }
-                    break;
-                case 2: // right
-                    if (player2.x < gameZone.getBoundingClientRect().right - player2.width - 3) {
-                        player2.el.style.backgroundImage = `url(${player2.sprites.right})`;
-                        player2.x += player2.step;
-                        player2.el.style.left = `${player.x}px`;
-                    }
-                    break;
-                case 4: // left
-                    if (player2.x > 0) {
-                        player2.el.style.backgroundImage = `url(${player2.sprites.left})`;
-                        player2.x -= player2.step;
-                        player2.el.style.left = `${player2.x}px`;
                     }
                     break;
                     
@@ -169,45 +112,7 @@ function intervals() {
      })
     }, fps);
 }
-ints.bullet2 = setInterval(() => {
-    let bullets2 = document.querySelectorAll('.bullet2');
-    bullets2.forEach((bullet2) => {
-        let direction2 = bullet2.getAttribute('direction2');
 
-        switch (direction2) {
-
-            case 'top':
-                if (bullet2.getBoundingClientRect().top < 0) {
-                    bullet2.parentNode.removeChild(bullet2);
-                } else {
-                    bullet2.style.top = bullet2.getBoundingClientRect().top - player2.bullet2.speed + 'px';
-                }
-                break;
-
-                case 'right':
-                if (bullet2.getBoundingClientRect().right > gameZone.getBoundingClientRect().width) {
-                    bullet2.parentNode.removeChild(bullet2);
-                } else {
-                    bullet2.style.left = bullet2.getBoundingClientRect().left + player2.bullet2.speed + 'px';
-                }
-                break;
-                case 'bottom':
-    if (bullet2.getBoundingClientRect().bottom > gameZone.getBoundingClientRect().height) {
-        bullet2.parentNode.removeChild(bullet2);
-    } else {
-        bullet2.style.top = bullet2.getBoundingClientRect().top + player2.bullet2.speed + 'px';
-    }
-    break;
-    case 'left':
-                if (bullet2.getBoundingClientRect().left < 0) {
-                    bullet2.parentNode.removeChild(bullet2);
-                } else {
-                    bullet2.style.left = bullet2.getBoundingClientRect().left - player2.bullet2.speed + 'px';
-                }
-                break;
-        }
-     })
-    }, fps);
 
 
 /*
@@ -231,23 +136,7 @@ function addBullet() {
     break;
    }
 
-   player2.el = document.querySelector('.player2');
-   switch (player.side) {
-    case 1:
-        gameZone.innerHTML += `<div class="bullet2" direction2="top" style="left: ${(player2.x + (player2.width / 2)) - 6.5}px; top: ${player2.y - 16}px;"></div>`;
-    break;
-    case 2:
-        gameZone.innerHTML += `<div class="bullet2" direction2="right" style="left: ${(player2.x + player2.width) - 6.5}px; top: ${player2.y + 30}px;"></div>`;
-    break;
-    case 3:
-        gameZone.innerHTML += `<div class="bullet2" direction2="bottom" style="left: ${(player2.x + (player2.width / 2)) - 7}px; bottom: ${gameZone.getBoundingClientRect().height - player2.y - player2.height}px;"></div>`;
-    break;
-    case 4:
-        gameZone.innerHTML += `<div class="bullet2" direction2="left" style="left: ${(player2.x) - 6.5}px; top: ${player2.y + player2.height / 2 - 10}px;"></div>`;
-    break;
-   }
-
-   player2.el = document.querySelector('.player2');
+   player.el = document.querySelector('.player');
 }
 
 /*
